640f278a0128dfb0c37fd903fd0468b8371805a3
rustam sagaddinov <rustiksagad@gmail.com>
2025-05-03 14:05:04 -0300

    first commit

---
+++ src/main/java/com/example/cloud_box/CloudBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloud_box;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBoxApplication.class, args);
+	}
+
+}
